97ad6a7cf45aecb8ac43f40b6a250bf9309e1f95
Lekesky <61468937+Lekesky@users.noreply.github.com>
2022-02-23 14:21:10 -0500

    double

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -29,26 +29,6 @@ class UserInput {
         System.out.println(message);
         String userInput = scanner.nextLine();
 
-        double userDouble = 0;
-        boolean isDouble = false;
-        while (!isDouble) {
-            try {
-                userDouble = Double.parseDouble(userInput);
-                isDouble = true;
-            }
-            catch (NumberFormatException e) {
-                System.out.println(userInput + " is not a valid double. " + message);
-                userInput = scanner.nextLine();
-            }
-        }
-
-        return userDouble;
-    }
-
-    public double promptString(String message) {
-        System.out.println(message);
-        String userInput = scanner.nextLine();
-
         double userDouble = 0.0;
         boolean isDouble = false;
         while (!isDouble) {
@@ -65,6 +45,26 @@ class UserInput {
         return userDouble;
     }
 
+    public String promptString(String message) {
+        System.out.println(message);
+        String userInput = scanner.nextLine();
+
+        String userString = "";
+        boolean isString = false;
+        while (!isString) {
+            try {
+                userString = String.format("");
+                isString = true;
+            }
+            catch (NumberFormatException e) {
+                System.out.println(userInput + " is not a valid string. " + message);
+                userInput = scanner.nextLine();
+            }
+        }
+
+        return userString;
+    }
+
 }
 
 
@@ -74,10 +74,13 @@ public class Main {
     public static void main(String[] args) {
         UserInput input = new UserInput();
         int aNumber = input.promptInt("Enter an integer.");
-        System.out.println(aNumber + " is valid");
+        System.out.println(aNumber + " is valid integer");
 
         double bNumber = input.promptDouble("Enter a double.");
-        System.out.println(bNumber + " is valid");
+        System.out.println(bNumber + " is a valid double");
+
+        String character = input.promptString("Enter a string.");
+        System.out.println(character + " is a valid string");
 
     }
 }
